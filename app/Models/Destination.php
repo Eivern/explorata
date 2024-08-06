134c2a89862434c0cc2012cade2e872544742215
Kemz <afilfriend@gmail.com>
2024-08-06 15:41:21 +0800

    feat: add destination model and resource

---
+++ app/Models/Destination.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\MultipleImageObserver;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[ObservedBy(MultipleImageObserver::class)]
+class Destination extends Model
+{
+    use HasFactory, Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country_id');
+    }
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class, 'state_id');
+    }
+}
---
+++ database/migrations/2024_08_06_025432_create_destinations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('country_id')->constrained('countries');
+            $table->foreignId('state_id')->constrained('states');
+            $table->string('name')->unique();
+            $table->string('address');
+            $table->json('images');
+            $table->text('description');
+            $table->string('slug')->unique();
+            $table->double('entrance_fee');
+            $table->unsignedBigInteger('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('destinations');
+    }
+};
