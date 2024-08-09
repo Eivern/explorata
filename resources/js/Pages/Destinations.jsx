60da9c10d55ac48fa1648834a77e6d39aec746cf
radwija <115165737+radwija@users.noreply.github.com>
2024-08-09 20:40:59 +0800

    add new links in auth layout

--- resources/js/Pages/Destinations.jsx
+++ resources/js/Pages/Destinations.jsx
@@ -11,7 +11,7 @@ export default function Destinations(props) {
   return (
     <>
       <Head title={props.title} />
-      <GuestLayout>
+      <GuestLayout user={props.auth.user}>
         <div className="min-h-screen py-16 main-container"
         >
           <div className="hidden md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
